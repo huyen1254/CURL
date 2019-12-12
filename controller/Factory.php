2248a31469e8b70eb65b9fd6eee828da1b0552f0
huyen125 <53432310+huyen1254@users.noreply.github.com>
2019-12-12 09:09:24 +0700

    addFactory

---
+++ controller/Factory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace controller;
+
+use model\ControlData;
+use Site\PagesFactory;
+
+class Factory extends ControllData
+{
+    public function getFactory($dataPage, PagesFactory $page)
+    {   
+        $keyPage = array(
+            'vnexpress', 'vietnamnet', 'dantri'
+        );
+         //set Value for Pages
+        foreach ($keyPage as $param) {
+            if (preg_match("/$param/", $dataPage['host'])) {
+                $page->html = $dataPage['html'];
+                $website = $page->makeWebsite($param);
+                $title = $website->getTitle();
+                $date = $website->getDate();
+                $content = $website->getContent();
+            }
+        }
+        echo '<h2> ' . $title . '</h2> ' . $date . '><br>' . $content;
+
+        $data = [
+            'host' => $dataPage['host'],
+            'path' => $dataPage['path'],
+            'title' => $title,
+            'content' => $content,
+
+            'date' => $date
+        ];
+
+        return $data;
+    }
+
+    public function addToTheDatabase($data)
+    {
+        $this->model->addPage($data['path'], $data['host'], $data['title'], $data['content'], $data['image'], $data['date']);
+    }
+}
