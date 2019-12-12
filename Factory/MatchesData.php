26d81274102e5dd8cf594cdaf47006ca06a2ae73
huyen125 <53432310+huyen1254@users.noreply.github.com>
2019-12-12 08:58:03 +0700

    updateMatchesData

---
+++ Factory/MatchesData.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Site;
+
+use Factory\MatchesData;
+use Factory\Dantri;
+use Factory\Vietnamnet;
+use Factory\Vnexpress;
+
+
+class PagesFactory
+{
+    public $html;
+
+    function makeWebsite(string $param)
+    {
+        switch (strtolower($param)) {
+            case 'vnexpress':
+                return new Vnexpress($this->html);
+                break;
+            case 'vietnamnet':
+                return new Vietnamnet($this->html);
+                break;
+            case 'dantri':
+                return new Dantri($this->html);
+                break;
+            default:
+                return null;
+                break;
+        }
+    }
+}
