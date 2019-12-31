3a1d5b99614dad931ee3e84c9ad2a04758d082a1
huyen125 <53432310+huyen1254@users.noreply.github.com>
2019-12-31 15:30:42 +0700

    addtheh2

--- View/index.php
+++ View/index.php
@@ -1,4 +1,5 @@
 <form method="POST" action="">
+         <h2>Create Crawler PHP</h2>
         <input type="text" value="" placeholder="" name="urlPages"><br>
         <button type="submit" name="submit">SUBMIT</button>
 </form>
