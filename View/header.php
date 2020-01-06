ead923945c0b9eb4694e4f059bf75fe947b8befe
huyen125 <53432310+huyen1254@users.noreply.github.com>
2020-01-06 11:04:14 +0700

    stylecss

--- View/header.php
+++ View/header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Crawler</title>
-<meta name="description" content="">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Crawler</title>
+    <link rel="stylesheet" type="text/css" href="public/css/style.css" />
 </head>
 <body>
 
