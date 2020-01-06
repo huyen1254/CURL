854615db1fb2e778eae6172a218fc6ae7e004a01
huyen125 <53432310+huyen1254@users.noreply.github.com>
2020-01-06 12:07:54 +0700

    addinclude

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -6,8 +6,9 @@ class HomeController
 {
     public function index()
     {
-       
+        include APP . "/View/header.php";
         include APP . "/View/index.php";
+        include APP . "/View/footer.php";
         
     }
 }
