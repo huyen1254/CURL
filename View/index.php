a946d3c6b91fdf19e972a5edf5755fda55baa12e
huyen125 <53432310+huyen1254@users.noreply.github.com>
2020-01-06 10:58:52 +0700

    editformseacrch

--- View/index.php
+++ View/index.php
@@ -1,5 +1,5 @@
-<form method="POST" action="">
-         <h2>Create Crawler PHP</h2>
-        <input type="text" value="" placeholder="" name="urlPages"><br>
-        <button type="submit" name="submit">SUBMIT</button>
+<form method="POST" class="example" action="/action_page.php" style="margin:auto;max-width:300px">
+    <h2>Create Crawler PHP</h2>
+    <input type="text" placeholder="Search.." name="urlPages">
+    <button type="submit"><i class="fa fa-search"></i></button>
 </form>
