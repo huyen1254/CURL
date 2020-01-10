8afbecdec52814b2342e580da14939c969544718
huyen125 <53432310+huyen1254@users.noreply.github.com>
2020-01-10 11:25:08 +0700

    add table

---
+++ View/table.php
@@ -0,0 +1,28 @@
+
+<table>
+    <thead>
+        <tr>
+            <th>MSSV</th>
+            <th>Họ & Tên</th>
+            <th>Ngày Sinh</th>
+            <th>Điểm Trung Bình</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+       
+    </tbody>
+</table>
+<style>
+    table tr td {
+    border-bottom: 2px solid #ccc;
+    padding-left: 15px;
+    padding-right: 15px;
+}
+</style>
